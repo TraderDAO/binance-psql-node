8d907a306c94ec9241e36696728237e7dc7a7903
pete <>
2022-08-02 19:07:42 +0800

    Refactor isNewfunction

--- Inputs/config.js
+++ Inputs/config.js
@@ -1,45 +1,41 @@
 const dbInput = {
-    markPriceTable: "markPrice",
-    orderstable:"orderstable",
-    loadInterval: 1000, 
+  markPriceTable: "markPrice",
+  orderstable: "orderstable",
+  loadInterval: 1000,
 };
 
 const tradingInput = {
-    market: "BTCBUSD",
-    timeframe: "1m",
-    exchange: "binance"
+  market: "BTCBUSD",
+  timeframe: "1m",
+  exchange: "binance",
 };
 
 const symbols = [
-    "CRV/BUSD",
-    // "ADA/BUSD",
-    "FTM/BUSD",
-    // "ETH/BUSD",
-    // "ETHUSDT",
-    // "BTCUSDT",
-    // "XRPUSDT",
-    // "ADAUSDT",
-    // "BATUSDT",
-    // "BNBUSDT",
-    // "LINKUSDT",
-    // "MANAUSDT",
-    // "SOLUSDT",
-    // "MATICUSDT",
-    // "THETAUSDT",
-    // "HNTUSDT",
-    // "DOTUSDT",
-    // "YFIUSDT",
-    // "LUNAUSDT",
-    // "UNIUSDT",
-    // "AVAXUSDT",
-    // "AAVEUSDT",
-    // "QNTUSDT",
-    // "AXSUSDT",
-    // "FILUSDT"
+  "CRV/BUSD",
+  // "ADA/BUSD",
+  "FTM/BUSD",
+  // "ETH/BUSD",
+  // "ETHUSDT",
+  // "BTCUSDT",
+  // "XRPUSDT",
+  // "ADAUSDT",
+  // "BATUSDT",
+  // "BNBUSDT",
+  // "LINKUSDT",
+  // "MANAUSDT",
+  // "SOLUSDT",
+  // "MATICUSDT",
+  // "THETAUSDT",
+  // "HNTUSDT",
+  // "DOTUSDT",
+  // "YFIUSDT",
+  // "LUNAUSDT",
+  // "UNIUSDT",
+  // "AVAXUSDT",
+  // "AAVEUSDT",
+  // "QNTUSDT",
+  // "AXSUSDT",
+  // "FILUSDT"
 ];
 
-export{
-    dbInput,
-    tradingInput,
-    symbols
-}
+export { dbInput, tradingInput, symbols };
--- utilities/isNewBar.js
+++ utilities/isNewBar.js
@@ -1,33 +1,30 @@
-import {binanceClient} from "../ExchangeSetting/exchangeConfig.js";
+import {binanceClient} from '../ExchangeSetting/exchangeConfig.js';
 
-let barNow = [0];
+const barNow = [0];
 
 const isNewBar = async (market, timeframe) => {
-        try{
-            const ohlc = await binanceClient.fetchOHLCV(market, timeframe, undefined, 2);
-
-            if(barNow[barNow.length - 1] != ohlc[0][1]){
-                barNow.push(ohlc[0][1]);
-                barNow.shift();
-                console.log("new bar:", barNow);
-                const barInfo = {};
-                barInfo.isNewBar = true;
-                barInfo.timestamp = ohlc[0][0];
-                return barInfo;
-            }else{
-                // console.log("same bar", barNow);
-                return false;
-            }
-        }catch(err){
-            return console.log("isNewBar err", err);
-        }
-}
+  try {
+    const ohlc = await binanceClient.fetchOHLCV(
+        market, timeframe, undefined, 2);
+    if (barNow[barNow.length - 1] != ohlc[0][1]) {
+      barNow.push(ohlc[0][1]);
+      barNow.shift();
+      console.log('new bar:', barNow);
+      const barInfo = {isNewBar: true, timestamp: ohlc[0][0]};
+      return barInfo;
+    }
+    // console.log("same bar", barNow);
+    return false;
+  } catch (err) {
+    return console.log('isNewBar err', err);
+  }
+};
 
 // setInterval(() => {
 //     isNewBar("BTC/BUSD", "1m")
 // }, 6000);
 
-export{
-    isNewBar,
-}
+export {
+  isNewBar,
+};
 
