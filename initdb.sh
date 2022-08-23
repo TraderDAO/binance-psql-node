aedf06b23750d83fa275bd87a2d4b32e60273ec3
pete <peteryuhsin@gmail.com>
2022-08-23 19:30:23 +0800

    Update load mark price method #22

--- initdb.sh
+++ initdb.sh
@@ -5,5 +5,5 @@ export PGOPTIONS
 
 psql -d traderDao -c "DROP TABLE IF EXISTS markprice, orderstable;"
 psql -d traderDao -c "CREATE SCHEMA IF NOT EXISTS dbt_traderdao;"
-psql -d traderDao -c "CREATE TABLE dbt_traderdao.markprice(id SERIAL PRIMARY KEY, timestamp BIGINT, symbol TEXT, price numeric);"
+psql -d traderDao -c "CREATE TABLE dbt_traderdao.markprice2(id SERIAL PRIMARY KEY, symbol TEXT, price numeric, timestamp BIGINT, datetime TEXT, receivetime TEXT, receiveTimestamp TEXT);"
 psql -d traderDao -c "CREATE TABLE dbt_traderdao.orderstable2(id SERIAL PRIMARY KEY, time BIGINT, orderId BIGINT, symbol TEXT, base TEXT, side TEXT, price numeric, amount numeric, cost numeric, executedQty numeric, remaining numeric, type TEXT, openStatus TEXT, unfilledStatus TEXT);"
