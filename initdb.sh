2e4b7307d95c0aeb4f24816aa57b79515a430eb6
pete <peteryuhsin@gmail.com>
2022-08-24 14:31:53 +0800

    Add portfolio, exchange, account id columns #23

--- initdb.sh
+++ initdb.sh
@@ -6,5 +6,5 @@ export PGOPTIONS
 psql -d traderDao -c "DROP TABLE IF EXISTS markprice, orderstable;"
 psql -d traderDao -c "CREATE SCHEMA IF NOT EXISTS dbt_traderdao;"
 psql -d traderDao -c "CREATE TABLE dbt_traderdao.markprice2(id SERIAL PRIMARY KEY, symbol TEXT, price numeric, timestamp BIGINT, datetime TEXT, receivetime TEXT, receiveTimestamp TEXT);"
-psql -d traderDao -c "CREATE TABLE dbt_traderdao.orderstable2(id SERIAL PRIMARY KEY, time BIGINT, orderId BIGINT, symbol TEXT, base TEXT, side TEXT, price numeric, amount numeric, cost numeric, executedQty numeric, remaining numeric, type TEXT, openStatus TEXT, unfilledStatus TEXT);"
 psql -d traderDao -c "CREATE TABLE dbt_traderdao.settlementprice(id SERIAL PRIMARY KEY, symbol TEXT, price numeric, timestamp BIGINT, datetime TEXT, receivetime TEXT, receiveTimestamp TEXT);"
+psql -d traderDao -c "CREATE TABLE dbt_traderdao.orderstable3(id SERIAL PRIMARY KEY, time BIGINT, orderId BIGINT, symbol TEXT, base TEXT, side TEXT, price numeric, amount numeric, cost numeric, executedQty numeric, remaining numeric, type TEXT, openStatus TEXT, unfilledStatus TEXT, portfolio_id numeric, account_id numeric, exchange_id numeric);"
