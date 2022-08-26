ef3264f923c3be3d8ce90c5b047bd7e3a867e03c
RueiCiWang <fatfingererr@gmail.com>
2022-08-26 19:11:06 +0800

    Deploy

--- initdb.sh
+++ initdb.sh
@@ -3,7 +3,7 @@
 PGOPTIONS="--search_path=public"
 export PGOPTIONS
 
-PGPASSWORD=3QIfWVSayB psql -h orangeskylab-1.c9ejcf8ctlf9.us-west-2.rds.amazonaws.com -p 5432 -U postgres -d postgres -w -c "DROP TABLE IF EXISTS markprice, settlementprice, orderstable;"
+PGPASSWORD=3QIfWVSayB psql -h orangeskylab-1.c9ejcf8ctlf9.us-west-2.rds.amazonaws.com -p 5432 -U postgres -d postgres -w -c "DROP TABLE CASCADE IF EXISTS markprice, settlementprice, orderstable;"
 PGPASSWORD=3QIfWVSayB psql -h orangeskylab-1.c9ejcf8ctlf9.us-west-2.rds.amazonaws.com -p 5432 -U postgres -d postgres -w -c "CREATE SCHEMA IF NOT EXISTS public;"
 PGPASSWORD=3QIfWVSayB psql -h orangeskylab-1.c9ejcf8ctlf9.us-west-2.rds.amazonaws.com -p 5432 -U postgres -d postgres -w -c "CREATE TABLE public.markprice(id SERIAL PRIMARY KEY, symbol TEXT, price numeric, timestamp BIGINT, datetime TEXT, receivetime TEXT, receiveTimestamp TEXT);"
 PGPASSWORD=3QIfWVSayB psql -h orangeskylab-1.c9ejcf8ctlf9.us-west-2.rds.amazonaws.com -p 5432 -U postgres -d postgres -w -c "CREATE TABLE public.settlementprice(id SERIAL PRIMARY KEY, symbol TEXT, price numeric, timestamp BIGINT, datetime TEXT, receivetime TEXT, receiveTimestamp TEXT);"
