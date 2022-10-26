98f07332dbf8caf27cfbd5726b89bcf3c35c8760
pete <peteryuhsin@gmail.com>
2022-10-26 17:11:42 +0800

    Resolve #24 #45 #46

---
+++ utilities/loadFormerSymbols.js
@@ -0,0 +1,17 @@
+import { symbols } from "../Inputs/config.js";
+import { receiveTime, receiveTimestamp } from "../utilities/timeNow.js";
+
+
+const loadFormerSymbol = async (pool) => {
+  const symbolArr = symbols;
+  symbolArr.forEach((symbol) => {
+    console.log(symbol)
+    const queryString = `INSERT INTO dbt_traderdao.activeasset(symbol, receivetime, receivetimestamp
+      )VALUES('${symbol}','${receiveTime()}', '${receiveTimestamp()}');`;
+    pool.query(queryString, (err) => {
+      if (err !== undefined) logger.error(`[initAsset] ${err}`);
+    });
+  });
+};
+
+export { loadFormerSymbol };
