05ea30f65e4bc906ae23a1ec61e8f8f6507ef2d9
pete <peteryuhsin@gmail.com>
2023-03-07 16:42:39 +0800

    add logger to userAssetGetter

--- Collector/userAssetGetter.js
+++ Collector/userAssetGetter.js
@@ -1,14 +1,13 @@
 import { binanceClient } from "../ExchangeSetting/exchangeConfig.js";
+import logger from "../logger.js";
 
-const getBalance = async() => {
-    try{
-        return (await binanceClient.fetchBalance()).total;
-    }catch(err){
-        logger.error(`[getBalance] ${err}`);
-        return console.log("getBalance err", err);
-    }
-}
+const getBalance = async () => {
+  try {
+    return (await binanceClient.fetchBalance()).total;
+  } catch (err) {
+    logger.error(`[getBalance] ${err}`);
+    return console.log("getBalance err", err);
+  }
+};
 
-export{
-    getBalance
-}
+export { getBalance };
